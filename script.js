368971a764a1d95dbdec516a3ef2d7418015f8b1
alpersilistre <alpersilistre@gmail.com>
2016-01-18 03:11:26 +0200

    New services added and $log used

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
   
   var app = angular.module("githubViewer", []);
   
-  var MainController = function($scope, $http){
+  var MainController = function($scope, $http, $interval, $log){
     
     var onUserComplete = function(response){
       $scope.user = response.data;
@@ -18,14 +18,34 @@
       $scope.error = "Could not fetch the data!";
     };
     
+    var decrementCountdown = function(){
+      $scope.countdown -= 1;
+      if($scope.countdown < 1){
+        $scope.search($scope.username);
+      }
+    };
+    
+    var countdownInterval = null; 
+    var startCountdown = function(){
+      countdownInterval = $interval(decrementCountdown, 1000, $scope.countdown);
+    };
+    
     $scope.search = function(username){
+      $log.info('Searching for ' + username);
       $http.get('https://api.github.com/users/' + username)
         .then(onUserComplete, onError);
+        
+      if(countdownInterval){
+        $interval.cancel(countdownInterval);
+        $scope.countdown = null;
+      }
     };
     
     $scope.username = 'alpersilistre';
     $scope.message = 'Github Viewer';
     $scope.repoSortOrder = '-stargazers_count';
+    $scope.countdown = 5;
+    startCountdown();
     
   };
   
